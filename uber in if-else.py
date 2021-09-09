7988ec759cc5f3658fbb09ece1a33bc410c59e45
Anjali74306 <89630072+Anjali74306@users.noreply.github.com>
2021-09-09 09:51:30 +0530

    Add files via upload

---
+++ uber in if-else.py
@@ -0,0 +1,34 @@
+print("WELCOM TO UBER APPLICATION")
+vechicles=input("ENTER WHICH VECHICLE YOU WANT")   
+if vechicles=="CAB" or "BIKE" or "AUTO":
+    print("NEXT")
+else: ("other vechicle is not there")
+
+charges=input("different uber differ charges for 1k/m")
+if charges=="CAB" or "BIKE" or "AUTO":
+        print("for Cab=250,Bike=75,Auto=150 ")
+else:("it is avilable for only 1k/m")    
+location=input("ENTER YOUR PICKUP LOCATION")
+print("next>")
+location2=input("WHERE YOU WANT TO GO")
+print("next")
+payment=input("ENETR YOUR PAYMENT TYPE")
+if payment=="CASH" or "BY CARD":
+    print("next>")
+else: ("card only master card")
+Driver_Details=input("THEIR ARE SOME DRIVER DETAIL")
+
+print("FOR CAB-Name=Mr.Rahul wagle" ," /n","Phone no=9717305678"
+"FOR BIKE-NAME=Mr.Prakash singh","/n"," Phone No-9718077517"
+"FOR AUTO-NAME=Mr.suresh prasad"," /n","Phone No-7982401040")
+
+Rider_Details=input("THEIR ARE SOME VECHIELS DETAILS ")
+print("CAB IS IN RED COLOUR"," n/"," CAB NUMBER-6780, THIRE IS ONLY 4 SEAT",
+"BIKE-IT IS IN BLACK & GREY COLOR"," /n"," PLAT NO-4577","AUTO-IT IS NORMAL AUTO","PLAT NO-7899")
+print("your uber is confrm for riding")
+
+
+
+
+
+
